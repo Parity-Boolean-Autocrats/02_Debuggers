44eb169c19e123b09568f31cc657a31064212b03
sreekeshiyer <sreekesh.subramanian@gmail.com>
2022-10-01 00:10:36 +0530

    🎉 Boilerplate Setup

---
+++ backend/core/general/routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from config import BASE_URL
+
+general = Blueprint("general", __name__)
+
+# Routes
+
+@general.route(f"{BASE_URL}/")
+def index():
+    return jsonify({"greeting": "Welcome to the API!"})
