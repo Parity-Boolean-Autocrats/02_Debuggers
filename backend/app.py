3c481a5662a41f374eb863ae62ab3edf97e519fb
VKrishna2090 <vkrishna2090@gmail.com>
2022-10-02 15:39:16 +0530

    Predict function coded

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
 
 app = Flask(__name__, static_folder="static", static_url_path="")
 
@@ -26,6 +29,57 @@ def services():
 @app.route("/contact-us")
 def contact():
     return render_template("contact-us.html")
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+
+    pred = {0:{'status':'Low','suggestions':[]}, 
+        1:{'status':'Moderate','suggestions':[]}, 
+        2:{'status':'High','suggestions':[]}}
+
+    age = request.form['age']
+    education = request.form['education']
+    institution = request.form['institution']
+    it = request.form['it']
+    location = request.form['location']
+    loadshed = request.form['loadshed']
+    fincon = request.form['fincon']
+    internet = request.form['internet']
+    network = request.form['network']
+    selflms = request.form['selflms']
+    duration = request.form['duration']
+    device = request.form['device']
+
+    df = pd.DataFrame({
+        'Age': {0: '21-25'},
+        'Education Level': {0: 'University'},
+        'Institution Type': {0: 'Non Government'},
+        'IT Student': {0: 'No'},
+        'Location': {0: 'Yes'},
+        'Load-shedding': {0: 'Low'},
+        'Financial Condition': {0: 'Mid'},
+        'Internet Type': {0: 'Wifi'},
+        'Network Type': {0: '4G'},
+        'Class Duration': {0: '3-6'},
+        'Self Lms': {0: 'No'},
+        'Device': {0: 'Tab'}})
+    
+
+    col1 = df["Age"].apply(lambda x: x.split("-")[0])
+    df1 = df.join(col1.to_frame(name="Lower limit Age"))
+    df1.drop(['Age'], axis = 1, inplace = True)
+    df1["Lower limit Age"] = df1["Lower limit Age"].astype(int)
+    scaler = OrdinalEncoder()
+    names = df1.columns
+    d = scaler.fit_transform(df1)
+
+    scaled_df = pd.DataFrame(d, columns=names)
+
+    pickled_model = pickle.load(open("model.pkl", "rb"))
+    y_pred = pickled_model.predict(scaled_df)
+
+    for x in y_pred:
+        return(pred[int(x)])
 
 
 if __name__ == "__main__":
